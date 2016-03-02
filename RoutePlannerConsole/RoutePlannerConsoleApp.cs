19de0e41bf13d610bd2f3114b9541f5f2adcaa02
matthias.keller5@students.fhnw.ch <matthias.keller5@students.fhnw.ch>
2016-03-02 12:50:25 +0100

    Initial commit, Lab01

---
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
+{
+    class RoutePlannerConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            var version = AssemblyName.GetAssemblyName("RoutePlannerConsole.exe").Version;
+            Console.WriteLine("Welcome to Routeplanner (Version " + version + ")");
+            var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
+            Console.WriteLine("{0}: {1}/{2}", wayPoint.Name, wayPoint.Latitude, wayPoint.Longitude);
+        }
+    }
+}
